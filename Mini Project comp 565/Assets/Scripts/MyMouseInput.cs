f8e420eb8b16e4a697c586b794efee30a5a55cbe
Jose-Sal <jose.salcedo.386@my.csun.edu>
2022-03-08 22:59:58 -0800

    New additions
    be able to change shape, change material, and be able to delete the shapes

--- Mini Project comp 565/Assets/Scripts/MyMouseInput.cs
+++ Mini Project comp 565/Assets/Scripts/MyMouseInput.cs
@@ -1,51 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyMouseInput : MonoBehaviour
 {
-    int index = 0;
+    //public Button b;
+    public int Mytype;
+    public int MyTexture;
+    public string tagname;
+    public Material[] ChangeMaterial;
+    Renderer rend;
+    public /*PrimitiveType*/ void ChangeType(int type /*out PrimitiveType MyTest*/)
+    {
+        Mytype = type;
+    }
 
-    
+    public void Texture(int Texture)
+    {
+        MyTexture = Texture;
+    }
 
+    //for transparency
+    RaycastHit RHit;
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hitInfo = new RaycastHit();  //stores the collision information with other gameobjects
 
-        string test = "change me";
-        //string Mytype = (ChangeType(test));
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+
+        //--------------------------------------------------------------------
+        Ray mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(mousePosition, out RHit, 50000f, (1<<8))){
+            transform.position = hitInfo.point;
+        }
+        //---------------------------------------------------------------------
         if (Input.GetMouseButtonUp(0))  // check if left button is pressed
         {
+
+
+
+
             // take mouse position, convert from screen space to world space, do a raycast, store output of raycast into 
             // hitInfo object ...
 
             #region Screen To World
-            RaycastHit hitInfo = new RaycastHit();  //stores the collision information with other gameobjects
+            //RaycastHit hitInfo = new RaycastHit();  //stores the collision information with other gameobjects
 
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            //bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             if (hit)
             {
-
+                var test = PrimitiveType.Cube;
+               
+                switch (Mytype)
+                {
+                    case 3:
+                        test = PrimitiveType.Capsule;
+                        tagname = "MyCapsule";
+                        break;
+                    case 2:
+                        test = PrimitiveType.Sphere;
+                        tagname = "MySphere";
+                        break;
+                    default:
+                        test = PrimitiveType.Cube;
+                        tagname = "MyCube";
+                        break;
+                }
                 #region HIDE
-                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.tag = "MyCube";
+                
+                var Obj = GameObject.CreatePrimitive(test);
+
+                //this is to change the material
+                rend=Obj.GetComponent<Renderer>();
+                rend.sharedMaterial = ChangeMaterial[MyTexture];
+
+                Obj.tag = tagname;
+                Obj.gameObject.AddComponent < BoxCollider >();
                 //cube.GetComponent<BoxCollider>().isTrigger = true;
                 //cube.GetComponent<Renderer>().material = blockMaterial;
+
                 #endregion
-               
+                
+
                 //cube.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + 0.5f, hitInfo.point.z);
                 #region HIDE
                 //once the ray hits an object with tag Base, we want to spawn in a cube in the location of the raycast hit
                 if (hitInfo.transform.tag.Equals("Base"))
                 {
-                    cube.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + (0.5f), hitInfo.point.z);
+                    Obj.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + (0.5f), hitInfo.point.z);
                 }
                 #region HIDE
                 //if it hits another cube then we must perfectly align itself to the surface it was hit
                 else
                 {
                     //we use normal to get the vector perpendicular of the surface. do it all for 6 of the surfaces
-                    cube.transform.position= hitInfo.transform.position + hitInfo.normal;
+                    Obj.transform.position= hitInfo.transform.position + hitInfo.normal;
                    
                 }
                 #endregion
@@ -62,53 +114,21 @@ public class MyMouseInput : MonoBehaviour
             }
             #endregion
         }
-    }
-
-    //public void Cube()
-    //{
-    //    Debug.Log("this is a Cube");
-    //    var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-    //    cube.tag = "MyCube";
-    //}
-    //public void Sphere()
-    //{
-    //    Debug.Log("this is a Sphere");
-    //    var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-    //    sphere.tag = "MySphere";
-    //}
-    //public void Capsule()
-    //{
-    //    Debug.Log("this is a Capsule");
-    //    var capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-    //    capsule.tag = "MyCapsule";
-    //}
-
-    public /*PrimitiveType*/ void ChangeType(string type)
-    {
-
-        switch (type)
+        else if (Input.GetMouseButtonDown(1))
         {
-            case "Cube":
-                print("i am cube");
-                //obType = PrimitiveType.Cube;
-                //return PrimitiveType.Cube;
-                break;
-            case "Sphere":
-                print("i am sphere");
-                PrimitiveType sphere = PrimitiveType.Sphere;
-                //return PrimitiveType.Sphere;
-                break;
-            case "Capsule":
-                print("i am capsule");
-                PrimitiveType Capsule = PrimitiveType.Capsule;
-                //return PrimitiveType.Capsule;
-                break;
-            default:
-                print("not selected");
-                //return PrimitiveType.Cube;
-                break;
+            if (hit) { 
+            if (hitInfo.transform.tag != "Base")
+            {
+                Destroy(hitInfo.collider.gameObject);
 
+            }
+            
+                return;
+            }
+            //else if (hitInfo.transform.tag.Equals("MySphere"))
+            //{
+            //    Debug.Log("im sphere");
+            //}
         }
     }
-
 }
